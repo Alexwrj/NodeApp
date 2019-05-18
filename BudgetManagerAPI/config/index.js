960ea31ea0a8449428e945f8e794a5f71c848307
Alexwrj <32782446+polygonator@users.noreply.github.com>
2019-05-18 11:32:11 +0300

    1

--- BudgetManagerAPI/config/index.js
+++ BudgetManagerAPI/config/index.js
@@ -3,13 +3,3 @@ module.exports = {
 	session: {session: false},
 	database: 'mongodb://127.0.0.1:27017/budgetmanager'
 }
-
-require('module-alias/register');
-const http = require('http'),
-	BudgetManagerAPI = require('@BudgetManagerAPI'),
-	BudgetManagerServer = http.Server(BudgetManagerAPI),
-	BudgetManagerPORT = process.env.PORT || 3001,
-	LOCAL = '0.0.0.0';
-
-BudgetManagerServer.listen(BudgetManagerPORT, LOCAL, 
-	() => console.log(`BudgetManagerAPI running on ${BudgetManagerPORT}`));
